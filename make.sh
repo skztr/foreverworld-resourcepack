6edf0e6e29e27e999d94541c159a4f0a035adb5b
skztr <skizotoro@gmail.com>
2019-11-15 13:27:50 +0000

    ignore gimp files that we use for editing

--- make.sh
+++ make.sh
@@ -3,4 +3,4 @@ orig="$PWD"
 base="$(dirname "${BASH_SOURCE[0]}")"
 cd "$base" || exit 1
 
-zip -r "$orig/foreverworld-resourcepack.zip" . -x '.git*' -x '*.zip' -x '*.sh'
+zip -r "$orig/foreverworld-resourcepack.zip" . -x '.git*' -x '*.zip' -x '*.sh' -x '*.xcf'
